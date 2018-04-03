238615996a008bdef93e2495e85c3725b7374666
gaurav <sharma02gaurav@gmail.com>
2018-04-03 18:21:34 +0530

    student register functionality

---
+++ server/model/student/register.js
@@ -0,0 +1,93 @@
+import { StudentSchema } from '../schemas';
+import database from '../../db';
+import {
+	ResponseUtility,
+	HashUtility,
+	RandomCodeUtility,
+} from '../../utility';
+import { EmailServices } from '../../services';
+
+
+const StudentModel = database.model('Student', StudentSchema);
+/**
+ * microservice to register a new student into the system.
+ * This will first check the students email existence
+ * Create a new user and sends a verification token via email
+ * @author gaurav sharma
+ * @since 3rd Apr 2018
+ *
+ * @param {String} name
+ * @param {String} email
+ * @param {String} address
+ * @param {*} picture
+ *
+ * @returns Promise
+ */
+export default ({
+	name,
+	email,
+	password,
+	address,
+	picture,
+	google = undefined,
+	facebook = undefined,
+}) => new Promise((resolve, reject) => {
+	if (name && ((email && password) || (google || facebook))) {
+		// check if the user already exists
+		const query = { email };
+		StudentModel.findOne(query)
+			.then(async (student) => {
+				if (student) {
+					reject(ResponseUtility.ERROR({ message: 'User already exists' }));
+				} else {
+					/**
+					 * check for image type,
+					 * if buffer then push to s3 otherwise
+					 * save the image url (in case of google and facebook),
+					 */
+
+					let encryptedPassword;
+					let verificationToken;
+					if (password) {
+						encryptedPassword = await HashUtility.generate(password);
+						// also generate the verification code for email verification
+						verificationToken = RandomCodeUtility();
+					}
+					// create a new student
+					const studentObject = new StudentModel({
+						name,
+						email,
+						password: encryptedPassword,
+						address,
+						picture,
+						google,
+						facebook,
+						profileCompleted: address ? true : false,
+						created: Date.now(),
+						isVerified: false,
+						deleted: false,
+						verificationToken,
+						verificationTokenTimestamp: Date.now(),
+					});
+
+					studentObject.save().then(() => {
+						// send verification email
+						if (email && password) {
+							// email /password register flow
+							const subject = `Welcome to tutable app ${name}!!`;
+							const text = `Your student account has been created. Your verification token is ${verificationToken}.`;
+							EmailServices({ to: email, text, subject })
+								.then(() => resolve(ResponseUtility.SUCCESS))
+								.catch(err => reject(ResponseUtility.ERROR({ message: 'Error sending verification mail.', error: err })));
+						} else {
+							resolve(ResponseUtility.SUCCESS_MESSAGE({ message: 'Created new user' }));
+						}
+						// resolve(ResponseUtility.SUCCESS);
+					});
+					// resolve();
+				}
+			}).catch(err => reject(ResponseUtility.ERROR({ message: 'Error looking for student', error: err })));
+	} else {
+		reject(ResponseUtility.MISSING_REQUIRED_PROPS);
+	}
+});
---
+++ tests/unit-tests/4student/1register.js
@@ -0,0 +1,22 @@
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import { StudentServices } from '../../../server/model';
+import { SUCCESS_CODE } from '../../../server/constants';
+
+export default () => {
+	describe('This will test the student register service', () => {
+		it('should register a student', (done) => {
+			const user = {
+				name: 'gaurav sharma',
+				email: 'sharma02gaurav@gmail.com',
+				password: 'password',
+			};
+			StudentServices.StudentRegisterService(user)
+				.then((success) => {
+					console.log(success);
+					done();
+				}).catch(err => done(err));
+			// done();
+		});
+	});
+};
