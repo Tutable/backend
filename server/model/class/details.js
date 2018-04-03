0b1ceb99f40ab0dc8097a1c18363f57595c53aaf
gaurav <sharma02gaurav@gmail.com>
2018-04-03 11:16:19 +0530

    class details now sends the review stats along with the last review content

--- server/model/class/details.js
+++ server/model/class/details.js
@@ -11,16 +11,15 @@ import { S3_TEACHER_CLASS } from '../../constants';
 const ClassModel = database.model('Classes', ClassSchema);
 const CategoryModel = database.model('Categories', CategorySchema);
 const TeacherModel = database.model('Teachers', TeacherSchema);
-// const ReviewModel = database.model('Reviews', ReviewSchema);
+const ReviewModel = database.model('Reviews', ReviewSchema);
 
 /**
  * microservice to get the details of the classes created by a
- * specific user. the id to be injected by the controller by
- * decoding headers
+ * specific user.
  * @author gaurav sharma
  * @since 28th March 2018
  *
- * @param {String} id is the id of teacher
+ * @param {String} classId is the id of class to fetch details
  * @returns Promise
  * @todo populate the details with the latest 5 reviews.
  */
@@ -32,59 +31,103 @@ export default ({ classId }) => new Promise((resolve, reject) => {
 		const categoryPopulation = { path: 'categoryName', model: CategoryModel, select: 'title parent' };
 		const teacherPopulation = { path: 'teacher', model: TeacherModel, select: 'name address picture' };
 
-		// const aggregationQuery = [{
-		// 	$unwind: '$ref',
-		// }, {
-		// 	$match: {
-		// 		deleted: false,
-		// 	},
-		// }, {
-		// 	$group: {
-		// 		_id: '$ref',
-		// 		count: { $sum: 1 },
-		// 	},
-		// }];
+		/**
+		 * this aggregation query will append the reviews stats associated with the requested class
+		 * like average review, review count and also sends the lastest review along with its data.
+		 * see $group query for that.
+		 */
+		const aggregationQuery = [
+			{ $unwind: '$ref' },
+			{ $match: { ref: classId, deleted: false } },
+			{
+				$project: {
+					_id: '$_id',
+					ref: '$ref',
+					by: '$by',
+					posted: '$posted',
+					review: '$review',
+					stars: '$stars',
+				},
+			}, {
+				$group: {
+					_id: '$ref',
+					avgStars: { $avg: '$stars' },
+					count: { $sum: 1 },
+					id: { $last: '$_id' },
+					review: { $last: '$review' },
+					stars: { $last: '$stars' },
+					by: { $last: '$by' },
+					posted: { $last: '$posted' },
+				},
+			},
+		];
 
-		// ReviewModel.aggregate(aggregationQuery)
-		// 	.exec((err, reviews) => {
-		// 		console.log(reviews);
-		// 	});
+		ReviewModel.aggregate(aggregationQuery)
+			.exec((err, reviews) => {
+				// console.log(reviews);
+				let matching = reviews.find(review => review._id === classId);
+				if (!matching) {
+					matching = {
+						avgStars: 0,
+						count: 0,
+					};
+				}
+				ClassModel.findOne(query, projection)
+					.populate(categoryPopulation)
+					.populate(teacherPopulation)
+					.then((success) => {
+						const {
+							_doc: {
+								_id,
+								name,
+								level,
+								bio,
+								timeline,
+								created,
+								cancelled,
+								payload,
+							},
+							$$populatedVirtuals: {
+								categoryName,
+								teacher,
+							},
+						} = success;
 
-		ClassModel.findOne(query, projection)
-			.populate(categoryPopulation)
-			.populate(teacherPopulation)
-			.then((success) => {
-				const {
-					_doc: {
-						_id,
-						name,
-						level,
-						bio,
-						timeline,
-						created,
-						cancelled,
-						payload,
-					},
-					$$populatedVirtuals: {
-						categoryName,
-						teacher,
-					},
-				} = success;
-
-				resolve(ResponseUtility.SUCCESS_DATA({
-					id: _id,
-					name,
-					teacher,
-					category: categoryName,
-					level,
-					bio,
-					timeline,
-					created,
-					cancelled,
-					payload: `${S3_TEACHER_CLASS}/${payload}`,
-				}));
-			})
-			.catch(err => reject(ResponseUtility.ERROR({ message: 'Error looking for details', error: err })));
+						const {
+							avgStars = 0,
+							count = 0,
+							id = undefined,
+							review = undefined,
+							posted = undefined,
+							by = undefined,
+							stars = undefined,
+						} = matching;
+						resolve(ResponseUtility.SUCCESS_DATA({
+							id: _id,
+							name,
+							teacher,
+							category: categoryName,
+							level,
+							bio,
+							timeline,
+							created,
+							cancelled,
+							payload: `${S3_TEACHER_CLASS}/${payload}`,
+							reviews: matching ? {
+								avgStars,
+								count,
+								lastReview: {
+									id,
+									review,
+									posted,
+									by,
+									stars,
+								},
+							} : undefined,
+						}));
+					})
+					.catch(err => reject(ResponseUtility.ERROR({ message: 'Error looking for details', error: err })));
+			});
 	} else {
 		reject(ResponseUtility.MISSING_REQUIRED_PROPS);
 	}
