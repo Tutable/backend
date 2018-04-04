34a863586c59e959f2e6b9a475ec89aa850bdfdb
gaurav <sharma02gaurav@gmail.com>
2018-04-04 16:28:34 +0530

    cleanups

--- server/utility/response.js
+++ server/utility/response.js
@@ -11,7 +11,7 @@ const SUCCESS_MESSAGE = ({
 }) => ({ code: 100, message, data });
 const SUCCESS_DATA = data => ({ code: 100, message: 'success', data });
 const SUCCESS_PAGINATION = (data, page, limit) => ({ code: 100, message: 'success', data, page, limit, length: data.length });
-const NOT_VERIFIED = {code: 105, message: 'User is not verified. Verify with the token sent via email or regenerate token.' };
+const NOT_VERIFIED = { code: 105, message: 'User is not verified. Verify with the token sent via email or regenerate token.' };
 const DATABASE_ERROR = error => ({ code: 103, message: 'Database error.', error });
 const ERROR = ({ error, message = 'error' }) => ({ code: 104, message, error });
 const USER_NOT_FOUND = { code: 104, message: 'Requested user not found' };
