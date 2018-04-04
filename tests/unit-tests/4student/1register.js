95660d133cfc2035596c049227c56e22bc1be325
gaurav <sharma02gaurav@gmail.com>
2018-04-04 12:19:31 +0530

    fix test

--- tests/unit-tests/4student/1register.js
+++ tests/unit-tests/4student/1register.js
@@ -11,7 +11,7 @@ export default () => {
 				email: 'sharma02gaurav@gmail.com',
 				password: 'password',
 			};
-			StudentServices.StudentRegisterService(user)
+			StudentServices.StudentsRegisterService(user)
 				.then((success) => {
 					console.log(success);
 					done();
