86fabebb6d1d3a0d972fd225764c570d62a4f2a8
gaurav <sharma02gaurav@gmail.com>
2018-04-05 14:33:59 +0530

    handlebars for email templates

--- server/services/templateMail.js
+++ server/services/templateMail.js
@@ -5,6 +5,7 @@
  */
 import fs from 'fs';
 import path from 'path';
+import handlebars from 'handlebars';
 import nodemailer from 'nodemailer';
 import { ResponseUtility } from '../utility';
 
@@ -25,10 +26,9 @@ const transporter = nodemailer.createTransport({
  * @param {String} text		-> email content
  * @param {String} subject	-> subject of email
  */
-export default ({ to, text, subject = 'Mail from tutable app' }) => new Promise((resolve, reject) => {
+const sendMail = ({ to, subject = 'Mail from tutable app', html }) => new Promise((resolve, reject) => {
 
 	// read html file here
-	const html = fs.readFileSync(path.resolve(__dirname, 'template', 'account_created.html'), { encoding: 'utf-8' });
 
 	transporter.sendMail({
 		from: user,
@@ -42,3 +42,40 @@ export default ({ to, text, subject = 'Mail from tutable app' }) => new Promise(
 		return resolve(ResponseUtility.SUCCESS);
 	});
 });
+// export default ({ to, text, subject = 'Mail from tutable app' }) => new Promise((resolve, reject) => {
+
+// 	// read html file here
+// 	const html = fs.readFileSync(path.resolve(__dirname, 'template', 'account_created.html'), { encoding: 'utf-8' });
+
+// 	transporter.sendMail({
+// 		from: user,
+// 		to,
+// 		html,
+// 		subject,
+// 	}, (err) => {
+// 		if (err) {
+// 			return reject(ResponseUtility.ERROR({ message: 'Error sending email.', error: err }));
+// 		}
+// 		return resolve(ResponseUtility.SUCCESS);
+// 	});
+// });
+
+/**
+ * send this email template for now account registering
+ * @param {String} to
+ */
+const NewAccountMail = ({ to, name, verificationCode }) => new Promise((resolve, reject) => {
+	const html = fs.readFileSync(path.resolve(__dirname, 'template', 'new_account_template.html'), { encoding: 'utf-8' });
+	const template = handlebars.compile(html);
+	const props = { name, verification_code: verificationCode };
+	const compiled = template(props);
+
+	sendMail({ to, subject: 'New account created', html: compiled })
+		.then(success => resolve(success))
+		.catch(err => reject(err));
+});
+
+export default {
+	NewAccountMail,
+};
+
