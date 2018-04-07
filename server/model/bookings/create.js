3033e147545859a05295f304849b98b6f19754c0
gaurav <sharma02gaurav@gmail.com>
2018-04-07 14:36:38 +0530

    updating users availability on success booking

--- server/model/bookings/create.js
+++ server/model/bookings/create.js
@@ -28,53 +28,90 @@ export default ({
 	slot,
 }) => new Promise((resolve, reject) => {
 	if (id && ref && slot) {
-		const teacherPopulation = { path: 'teacher', model: TeacherModel, select: 'name address picture email' };
+		/**
+		 * get the teacher id to fetch the teacher available slots and
+		 * verify whether the provided slot is valid or not.
+		 */
+
+		const teacherPopulation = { path: 'teacher', model: TeacherModel, select: 'name email availability' };
 		// check if user has already enrolled in this class
-		const checkQuery = { $and: [{ by: id }, { ref }] };
-		BookingsModel.findOne(checkQuery)
-			.then((booking) => {
-				if (booking) {
-					return reject(ResponseUtility.ERROR({ message: 'Already enrolled for this class.' }));
+		// const checkQuery = { $and: [{ by: id }, { ref }] };
+		// fetch the class details along with teacher email
+		// to send email verification about the class request
+
+		ClassModel.findOne({ _id: ref })
+			.populate(teacherPopulation)
+			.then((classDetails) => {
+				const {
+					_doc,
+					$$populatedVirtuals: {
+						teacher: {
+							_id,
+							name,
+							email,
+							availability,
+						},
+					},
+				} = classDetails;
+
+				if (!availability) {
+					return reject(ResponseUtility.ERROR({ message: 'Teacher is not available for the specified time slot.' }));
 				}
-				// fetch the class details along with teacher email
-				// to send email verification about the class request
 
-				ClassModel.findOne({ _id: ref })
-					.populate(teacherPopulation)
-					.then((classDetails) => {
-						const {
-							_doc,
-							$$populatedVirtuals: {
-								teacher: {
-									_id,
-									name,
-									email,
-								},
-							},
-						} = classDetails;
-						const bookingObject = new BookingsModel({
-							by: id,
-							ref,
-							teacher: _id,
-							slot,
-							timestamp: Date.now(),
-							deleted: false,
-							confirmed: false,
-							cancelled: false,
-							completed: false,
-						});
+				const requestedSlot = Object.keys(slot)[0];
+				const slotTimestamp = Object.keys(availability).find(timestamp => timestamp === requestedSlot);
+				if (!slotTimestamp) {
+					return reject(ResponseUtility.ERROR({ message: 'Teacher is not available for specified time slot.' }));
+				}
+				// if here then timestamp is valid.
+				// check if timestamp is greater than the cureent timestamp
+				if (Number(slotTimestamp) < Date.now()) {
+					return reject(ResponseUtility.ERROR({ message: 'Teacher is not avaialble for speified time slot.' }));
+				}
+				const containsSlot = availability[slotTimestamp].find(indSlot => indSlot.toString() === slot[requestedSlot].toString());
 
-						bookingObject.save()
+				if (!containsSlot) {
+					return reject(ResponseUtility.ERROR({ message: 'Teacher not available for specified time slot.' }));
+				}
+				// console.log('Yes. It contains slot.');
+				// add a new booking
+				const bookingObject = new BookingsModel({
+					by: id,
+					ref,
+					teacher: _id,
+					slot,
+					timestamp: Date.now(),
+					deleted: false,
+					confirmed: false,
+					cancelled: false,
+					completed: false,
+				});
+
+				bookingObject.save()
+					.then(() => {
+						EmailServices({ to: email, subject: `Request to attend ${_doc.name} class`, text: `${name} has requested you to attend ${_doc.name} class.` })
 							.then(() => {
-								EmailServices({ to: email, subject: `Request to attend ${_doc.name} class`, text: `${name} has requested you to attend ${_doc.name} class.` })
-									.then(() => resolve(ResponseUtility.SUCCESS))
-									.catch(err => resolve(ResponseUtility.ERROR({ message: 'Error sending email to teacher', error: err })));
-								// resolve(ResponseUtility.SUCCESS);
+								// trigger removing the assigned slot from the teachers
+								// availability
+								const availabilityObject = Object.assign({}, availability);
+								availabilityObject[requestedSlot].splice(availabilityObject[requestedSlot].indexOf(slot[requestedSlot].toString()), 1);
+								// console.log(availabilityObject);
+								TeacherModel.update({ _id }, { availability })
+									.then((modified) => {
+										const { nModified } = modified;
+										if (nModified) {
+											resolve(ResponseUtility.SUCCESS);
+										} else {
+											resolve(ResponseUtility.ERROR({ message: 'Nothing modified' }));
+										}
+									}).catch(err => reject(ResponseUtility.ERROR({ message: '', error: err })));
 							})
-							.catch(err => reject(ResponseUtility.ERROR({ message: 'Error saving booking', error: err })));
+							.catch(err => resolve(ResponseUtility.ERROR({ message: 'Error sending email to teacher', error: err })));
+						// resolve(ResponseUtility.SUCCESS);
 					})
-					.catch(err => reject(ResponseUtility.ERROR({ message: 'Error populating teacher details.', error: err })));
-			}).catch(err => reject(ResponseUtility.ERROR({ message: 'Error looking for earlier bookings.', error: err })));
+					.catch(err => reject(ResponseUtility.ERROR({ message: 'Error saving booking', error: err })));
+			})
+			.catch(err => reject(ResponseUtility.ERROR({ message: 'Error populating teacher details.', error: err })));
 	} else {
 		reject(ResponseUtility.MISSING_REQUIRED_PROPS);
 	}
