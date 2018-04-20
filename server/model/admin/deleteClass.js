29cf39cdd90b6ed85f929f53428b10646dfd945c
gaurav <sharma02gaurav@gmail.com>
2018-04-20 13:19:59 +0530

    cleanups

--- server/model/admin/deleteClass.js
+++ server/model/admin/deleteClass.js
@@ -12,7 +12,6 @@ const ClassModel = database.model('Classes', ClassSchema);
  * @param {Boolean} deleted
  */
 export default ({ classId, deleted = true }) => new Promise(async (resolve, reject) => {
-	console.log(classId, deleted);
 	if (classId) {
 		const lookupQuery = { _id: classId };
 		const updateQuery = { deleted };
